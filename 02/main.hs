2aaea72a2ec49c84edca37e5db2929af33245901
JT <jt@usekojo.com>
2024-12-02 19:22:47 -0800

    problem 2 part 2

--- 02/main.hs
+++ 02/main.hs
@@ -1,7 +1,5 @@
 import System.IO
 
-data Result = Safe | Unsafe deriving (Eq, Show)
-
 isSafeDiff :: Int -> Int -> Bool
 isSafeDiff a b = do
   let diff = abs (a - b)
@@ -13,13 +11,23 @@ areMonotonicallyIncreasingOrDecreasing [x] = True
 areMonotonicallyIncreasingOrDecreasing [x, y] = x < y || x > y
 areMonotonicallyIncreasingOrDecreasing (x:y:z:xs) = (x < y && y < z) || (x > y && y > z) && areMonotonicallyIncreasingOrDecreasing (y:z:xs)
 
-getResult :: [Int] -> Result
-getResult [] = Safe
-getResult [x] = Safe
-getResult [x, y] = if isSafeDiff x y then Safe else Unsafe
-getResult (x:y:z:xs) = do
-  if areMonotonicallyIncreasingOrDecreasing [x, y, z] && isSafeDiff x y then getResult (y:z:xs) else Unsafe
+isSafe :: [Int] -> Bool
+isSafe [] = True
+isSafe [x] = True
+isSafe [x, y] = if isSafeDiff x y then True else False
+isSafe (x:y:z:xs) = do
+  areMonotonicallyIncreasingOrDecreasing [x, y, z] && isSafeDiff x y && isSafe (y:z:xs)
 
+isSafe2 :: [Int] -> Bool
+isSafe2 [] = True
+isSafe2 [x] = True
+isSafe2 [x, y] = if isSafeDiff x y then True else False
+isSafe2 xs = do
+  any ((== True) . isSafe) (generateAllSublists xs)
+
+generateAllSublists :: [Int] -> [[Int]]
+generateAllSublists [] = []
+generateAllSublists (x:xs) = xs : map (x:) (generateAllSublists xs)
 
 main = do
   handle <- openFile "02/input.txt" ReadMode
@@ -28,6 +36,10 @@ main = do
   let records = lines contents
   let sequences = map (map read . words) records :: [[Int]]
 
-  let allResults = map getResult sequences
-  let part1 = length (filter (== Safe) allResults)
+  let allResults = map isSafe sequences
+  let part1 = length (filter (== True) allResults)
   print part1
+
+  let allResults2 = map isSafe2 sequences
+  let part2 = length (filter (== True) allResults2)
+  print part2
